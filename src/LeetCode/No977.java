7fc83424d5fb81f2be6eb2b6dc85c2727b3f44e5
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2020-10-16 10:35:44 +0800

    977

---
+++ src/LeetCode/No977.java
@@ -0,0 +1,38 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+/*
+给定一个按非递减顺序排序的整数数组 A，返回每个数字的平方组成的新数组，要求也按非递减顺序排序。
+
+ 
+
+示例 1：
+
+输入：[-4,-1,0,3,10]
+输出：[0,1,9,16,100]
+示例 2：
+
+输入：[-7,-3,2,3,11]
+输出：[4,9,9,49,121]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/squares-of-a-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class No977 {
+
+
+    public static void main(String[] args) {
+
+    }
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = A[i] * A[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
