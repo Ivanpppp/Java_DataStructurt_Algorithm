5fab7f607d17d4f05366845e09b761bc6646e86b
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2020-11-17 09:01:36 +0800

    No328

--- src/Ivan/test.java
+++ src/Ivan/test.java
@@ -1,8 +1,26 @@
 package Ivan;
 
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
-        int n = 4;
-        System.out.println("n = " + ((n > 4) ?99.9:9));
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        int count = 0;
+        if (n > Integer.MAX_VALUE || n < Integer.MIN_VALUE){
+            System.out.println(0);
+            return;
+        }
+        StringBuffer s = new StringBuffer();
+        while (n!=0){
+            s.append(n%2);
+            if (n%2 == 1){
+                count++;
+            }
+            n = n/2;
+        }
+        System.out.println(count);
     }
 }
+
