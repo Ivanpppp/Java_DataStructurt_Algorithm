240e7123232c9e762a8c80ab21f8b2d61789ed77
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2020-12-09 15:14:33 +0800

    No62

---
+++ src/LeetCode/No62.java
@@ -0,0 +1,68 @@
+package LeetCode;
+
+/*
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+
+问总共有多少条不同的路径？
+
+ 
+
+示例 1：
+
+
+输入：m = 3, n = 7
+输出：28
+示例 2：
+
+输入：m = 3, n = 2
+输出：3
+解释：
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向右 -> 向下
+2. 向右 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向右
+示例 3：
+
+输入：m = 7, n = 3
+输出：28
+示例 4：
+
+输入：m = 3, n = 3
+输出：6
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-paths
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class No62 {
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 2;
+        System.out.println(uniquePaths(3,7));
+    }
+
+    public static int uniquePaths(int m, int n) {
+        // 动态规划
+        // 首先初始化网格 —— 网格的左侧和上侧都为1，因为只能向下或者向右走
+        int[][] grid = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            grid[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            grid[0][i] = 1;
+        }
+        
+        // 随后开始遍历网格
+        // dp状态方程为 f(i,j) = f(i-1,j) + f(i,j-1)
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+            }
+        }
+        return grid[m-1][n-1];
+
+    }
+}
