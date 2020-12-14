d6e3776ab952aedc4a21038b11fa6606eaa54000
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2020-12-14 11:34:57 +0800

    No49

---
+++ src/LeetCode/No49.java
@@ -0,0 +1,47 @@
+package LeetCode;
+
+/*
+给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+
+示例:
+
+输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+说明：
+
+所有输入均为小写字母。
+不考虑答案输出的顺序。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/group-anagrams
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+import java.util.*;
+
+public class No49 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // 用Map来存储键值，对每个str中每个值进行排序，key存储其排序后的顺序str
+        Map<String,List<String>> map = new HashMap<>();
+        for (String str :
+                strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String new_str = new String(arr);
+            List<String> list = map.getOrDefault(new_str,new ArrayList<>());
+            list.add(str);
+            map.put(new_str,list);
+
+        }
+        return new ArrayList<List<String>>(map.values());
+
+    }
+}
