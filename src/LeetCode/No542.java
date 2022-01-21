95e5372cff79ddf5911ddee51b8ff65dbd8418e0
Ivanpppp <55040118+Ivanpppp@users.noreply.github.com>
2022-01-21 11:06:40 +0800

    1
    1

---
+++ src/LeetCode/No542.java
@@ -0,0 +1,68 @@
+package LeetCode;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给定一个由 0 和 1 组成的矩阵 mat ，请输出一个大小相同的矩阵，其中每一个格子是 mat 中对应位置元素到最近的 0 的距离。
+ *
+ * 两个相邻元素间的距离为 1 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：mat = [[0,0,0],[0,1,0],[0,0,0]]
+ * 输出：[[0,0,0],[0,1,0],[0,0,0]]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/01-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class No542 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 广度优先算法
+     * @param mat
+     * @return
+     */
+    public int[][] updateMatrix(int[][] mat) {
+        int[] dx = {0,-1,1,0};
+        int[] dy = {-1,0,0,1};
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] ans = new int[n][m];
+        boolean[][] seen = new boolean[n][m];
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0){
+                    queue.offer(new int[]{i,j});
+                    seen[i][j] = true;
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int x = poll[0];
+            int y = poll[1];
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i];
+                int my = y + dy[i];
+                if (mx >= 0 && my >= 0 && mx < n && my < m && !seen[mx][my]){
+                    ans[mx][my] = ans[x][y] + 1;
+                    seen[mx][my] = true;
+                    queue.offer(new int[]{mx,my});
+                }
+            }
+        }
+        return ans;
+
+    }
+}
---
+++ src/LeetCode/No994.java
@@ -0,0 +1,74 @@
+package LeetCode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 在给定的网格中，每个单元格可以有以下三个值之一：
+ *
+ * 值 0 代表空单元格；
+ * 值 1 代表新鲜橘子；
+ * 值 2 代表腐烂的橘子。
+ * 每分钟，任何与腐烂的橘子（在 4 个正方向上）相邻的新鲜橘子都会腐烂。
+ *
+ * 返回直到单元格中没有新鲜橘子为止所必须经过的最小分钟数。如果不可能，返回 -1。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotting-oranges
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class No994 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 广度优先遍历
+     * 每次都会让周围的橘子坏掉，满足广度优先遍历的情况
+     * 如果最后还有橘子没有腐烂，则返回-1
+     *      说明需要用一个count来表示所有的橘子数
+     *      如果最后count ！= 0 说明还有橘子没有腐烂
+     * @param grid
+     * @return
+     */
+    public int orangesRotting(int[][] grid) {
+        int[] dx = {0,-1,1,0};
+        int[] dy = {-1,0,0,1};
+        if (grid == null || grid.length == 0){
+            return 0;
+        }
+        int ans = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2){
+                    queue.offer(new int[]{i,j});
+                } else if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        while (count > 0 && !queue.isEmpty()){
+            int s = queue.size();;
+            for (int j = 0; j < s; j++) {
+                int[] poll = queue.poll();
+                int x = poll[0];
+                int y = poll[1];
+                for (int i = 0; i < 4; i++) {
+                    int mx = x + dx[i];
+                    int my = y + dy[i];
+                    if (mx < n && my < m && mx >= 0 && my >= 0 && grid[mx][my] == 1){
+                        grid[mx][my] = 2;
+                        count--;
+                        queue.offer(new int[]{mx,my});
+                    }
+                }
+            }
+            ans++;
+        }
+        return count > 0 ? -1 : ans;
+    }
+}
